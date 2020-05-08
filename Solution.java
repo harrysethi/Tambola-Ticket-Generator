b906f7f51dc8db0adb6886e4e72e0e61f6353de3
Harinder Sethi <sethi.harinder@gmail.com>
2020-05-09 02:40:22 +0530

    Quick patch to ensure columns are sorted

--- Solution.java
+++ Solution.java
@@ -1,11 +1,9 @@
+import java.io.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-
-/**
- * 
- */
 
 /**
  * @author harinder
@@ -36,6 +34,72 @@ public class Solution {
 			}
 			
 			return count;
+		}
+		
+		// gives the row number of first found empty cell in given column
+		public int getEmptyCellInCol(int c) {
+			for(int i=0; i<3; i++){
+				if(A[i][c] == 0) return i;
+			}
+			
+			return -1;
+		}
+		
+		private void sortColumnWithThreeNumbers(int c) throws Exception {
+			int emptyCell = this.getEmptyCellInCol(c);
+			if (emptyCell != -1) {
+				throw new Exception("Sorry dude, your column has <3 cells, invalid function called");
+			}
+			
+			int tempArr[] = new int [] {this.A[0][c], this.A[1][c], this.A[2][c]};
+			Arrays.sort(tempArr);
+			
+			for (int r=0; r<3; r++)
+			{
+				this.A[r][c] = tempArr[r];
+			}
+		}
+		
+		private void sortColumnWithTwoNumbers(int c) throws Exception {
+			int emptyCell = this.getEmptyCellInCol(c);
+			if (emptyCell == -1) {
+				throw new Exception("Sorry dude, your column has 3 cells, invalid function called");
+			}
+			
+			int cell1, cell2;
+			if (emptyCell == 0) { cell1 = 1; cell2 = 2; }
+			else if (emptyCell == 1) { cell1 = 0; cell2 = 2; }
+			else { cell1 = 0; cell2 = 1; } // emptyCell == 2
+			
+			if (this.A[cell1][c] < this.A[cell2][c]) { 
+				return; 
+			}
+			else {
+				// swap
+				int temp = this.A[cell1][c];
+				this.A[cell1][c] = this.A[cell2][c];
+				this.A[cell2][c] = temp;
+			}
+		}
+		
+		/*
+		 * Three possible scenarios:
+		 * 1) only one number in the column - leave
+		 * 2) Two numbers in the column & not sorted - swap
+		 * 3) Three numbers in the column - sort
+		 */
+		private void sortColumn(int c) throws Exception {
+			if (this.getColCount(c) == 1) { return; }
+			
+			else if (this.getColCount(c) == 2) { this.sortColumnWithTwoNumbers(c); }
+			
+			else { this.sortColumnWithThreeNumbers(c); }
+		}
+		
+		public void sortColumns() throws Exception {
+			for (int c=0; c<9; c++) {
+				this.sortColumn(c);
+			}
 		}
 	}
 	
@@ -268,6 +332,22 @@ public class Solution {
 			}		
 		}
 		
+		try
+		{
+			// quick patch to ensure columns are sorted
+			for (int i=0; i<6; i++) {
+				Node currTicket = nodes[i];
+				currTicket.sortColumns();
+			}
+		}
+		catch (Exception e)
+		{
+			// something wrong, not a P0...eating the exception
+			System.out.println("Note: there is a small probability your columns may not be sorted, it should not impact the gameplay");
+			System.out.println("Please create an issue in the github for investigation");
+			System.out.println(e.getMessage());
+		}
+
 		//print the tickets
 		for(int i=0;i<6;i++){
 			Node currTicket = nodes[i];
@@ -289,5 +369,4 @@ public class Solution {
 			}
 		}
 	}
-
 }
