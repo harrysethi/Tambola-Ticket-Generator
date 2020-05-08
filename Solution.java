e7123ceec7cc4de85ffc67bed2564c8d3648dd4a
Harinder Sethi <sethi.harinder@gmail.com>
2020-05-09 02:44:54 +0530

    just formatting change, no functionality change

--- Solution.java
+++ Solution.java
@@ -1,4 +1,3 @@
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -10,107 +9,122 @@ import java.util.Random;
  *
  */
 public class Solution {
-	
-	static class Node{
+
+	static class Node {
 		int A[][];
-		
-		public Node(){
+
+		public Node() {
 			this.A = new int[3][9];
 		}
-		
-		public int getRowCount(int r){
+
+		public int getRowCount(int r) {
 			int count = 0;
-			for(int i=0;i<9;i++){
-				if(A[r][i]!=0) count++;
+			for (int i = 0; i < 9; i++) {
+				if (A[r][i] != 0)
+					count++;
 			}
-			
+
 			return count;
 		}
-		
-		public int getColCount(int c){
+
+		public int getColCount(int c) {
 			int count = 0;
-			for(int i=0;i<3;i++){
-				if(A[i][c]!=0) count++;
+			for (int i = 0; i < 3; i++) {
+				if (A[i][c] != 0)
+					count++;
 			}
-			
+
 			return count;
 		}
-		
+
 		// gives the row number of first found empty cell in given column
 		public int getEmptyCellInCol(int c) {
-			for(int i=0; i<3; i++){
-				if(A[i][c] == 0) return i;
+			for (int i = 0; i < 3; i++) {
+				if (A[i][c] == 0)
+					return i;
 			}
-			
+
 			return -1;
 		}
-		
+
 		private void sortColumnWithThreeNumbers(int c) throws Exception {
 			int emptyCell = this.getEmptyCellInCol(c);
 			if (emptyCell != -1) {
 				throw new Exception("Sorry dude, your column has <3 cells, invalid function called");
 			}
-			
-			int tempArr[] = new int [] {this.A[0][c], this.A[1][c], this.A[2][c]};
+
+			int tempArr[] = new int[] { this.A[0][c], this.A[1][c], this.A[2][c] };
 			Arrays.sort(tempArr);
-			
-			for (int r=0; r<3; r++)
-			{
+
+			for (int r = 0; r < 3; r++) {
 				this.A[r][c] = tempArr[r];
 			}
 		}
-		
+
 		private void sortColumnWithTwoNumbers(int c) throws Exception {
 			int emptyCell = this.getEmptyCellInCol(c);
 			if (emptyCell == -1) {
 				throw new Exception("Sorry dude, your column has 3 cells, invalid function called");
 			}
-			
+
 			int cell1, cell2;
-			if (emptyCell == 0) { cell1 = 1; cell2 = 2; }
-			else if (emptyCell == 1) { cell1 = 0; cell2 = 2; }
-			else { cell1 = 0; cell2 = 1; } // emptyCell == 2
-			
-			if (this.A[cell1][c] < this.A[cell2][c]) { 
-				return; 
+			if (emptyCell == 0) {
+				cell1 = 1;
+				cell2 = 2;
+			} else if (emptyCell == 1) {
+				cell1 = 0;
+				cell2 = 2;
+			} else { // emptyCell == 2
+				cell1 = 0;
+				cell2 = 1;
 			}
-			else {
+
+			if (this.A[cell1][c] < this.A[cell2][c]) {
+				return;
+			} else {
 				// swap
 				int temp = this.A[cell1][c];
 				this.A[cell1][c] = this.A[cell2][c];
 				this.A[cell2][c] = temp;
 			}
 		}
-		
+
 		/*
-		 * Three possible scenarios:
-		 * 1) only one number in the column - leave
-		 * 2) Two numbers in the column & not sorted - swap
+		 * Three possible scenarios: 
+		 * 1) only one number in the column - leave 
+		 * 2) Two numbers in the column & not sorted - swap 
 		 * 3) Three numbers in the column - sort
 		 */
 		private void sortColumn(int c) throws Exception {
-			if (this.getColCount(c) == 1) { return; }
-			
-			else if (this.getColCount(c) == 2) { this.sortColumnWithTwoNumbers(c); }
-			
-			else { this.sortColumnWithThreeNumbers(c); }
+			if (this.getColCount(c) == 1) {
+				return;
+			}
+
+			else if (this.getColCount(c) == 2) {
+				this.sortColumnWithTwoNumbers(c);
+			}
+
+			else {
+				this.sortColumnWithThreeNumbers(c);
+			}
 		}
-		
+
 		public void sortColumns() throws Exception {
-			for (int c=0; c<9; c++) {
+			for (int c = 0; c < 9; c++) {
 				this.sortColumn(c);
 			}
 		}
 	}
-	
-	static int getRand(int min, int max){
+
+	static int getRand(int min, int max) {
 		Random rand = new Random();
 		return rand.nextInt(max - min + 1) + min;
 	}
-	
-	static int getNumberOfElementsInSet(List<List<Integer>> set){
+
+	static int getNumberOfElementsInSet(List<List<Integer>> set) {
 		int count = 0;
-		for(List<Integer> li : set) count += li.size();
+		for (List<Integer> li : set)
+			count += li.size();
 		return count;
 	}
 
@@ -118,56 +132,56 @@ public class Solution {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Node []nodes = new Node[6];
-		for(int i=0;i<6;i++){
+		Node[] nodes = new Node[6];
+		for (int i = 0; i < 6; i++) {
 			nodes[i] = new Node();
 		}
-		
+
 		List<Integer> l1 = new ArrayList<Integer>();
-		for(int i=1;i<=9;i++){
+		for (int i = 1; i <= 9; i++) {
 			l1.add(i);
 		}
-		
+
 		List<Integer> l2 = new ArrayList<Integer>();
-		for(int i=10;i<=19;i++){
+		for (int i = 10; i <= 19; i++) {
 			l2.add(i);
 		}
-		
+
 		List<Integer> l3 = new ArrayList<Integer>();
-		for(int i=20;i<=29;i++){
+		for (int i = 20; i <= 29; i++) {
 			l3.add(i);
 		}
-		
+
 		List<Integer> l4 = new ArrayList<Integer>();
-		for(int i=30;i<=39;i++){
+		for (int i = 30; i <= 39; i++) {
 			l4.add(i);
 		}
-		
+
 		List<Integer> l5 = new ArrayList<Integer>();
-		for(int i=40;i<=49;i++){
+		for (int i = 40; i <= 49; i++) {
 			l5.add(i);
 		}
-		
+
 		List<Integer> l6 = new ArrayList<Integer>();
-		for(int i=50;i<=59;i++){
+		for (int i = 50; i <= 59; i++) {
 			l6.add(i);
 		}
-		
+
 		List<Integer> l7 = new ArrayList<Integer>();
-		for(int i=60;i<=69;i++){
+		for (int i = 60; i <= 69; i++) {
 			l7.add(i);
 		}
-		
+
 		List<Integer> l8 = new ArrayList<Integer>();
-		for(int i=70;i<=79;i++){
+		for (int i = 70; i <= 79; i++) {
 			l8.add(i);
 		}
-		
+
 		List<Integer> l9 = new ArrayList<Integer>();
-		for(int i=80;i<=90;i++){
+		for (int i = 80; i <= 90; i++) {
 			l9.add(i);
 		}
-		
+
 		List<List<Integer>> columns = new ArrayList<List<Integer>>();
 		columns.add(l1);
 		columns.add(l2);
@@ -178,16 +192,15 @@ public class Solution {
 		columns.add(l7);
 		columns.add(l8);
 		columns.add(l9);
-		
-		
+
 		List<List<Integer>> set1 = new ArrayList<List<Integer>>();
 		List<List<Integer>> set2 = new ArrayList<List<Integer>>();
 		List<List<Integer>> set3 = new ArrayList<List<Integer>>();
 		List<List<Integer>> set4 = new ArrayList<List<Integer>>();
 		List<List<Integer>> set5 = new ArrayList<List<Integer>>();
 		List<List<Integer>> set6 = new ArrayList<List<Integer>>();
-		
-		for(int i=0;i<9;i++){
+
+		for (int i = 0; i < 9; i++) {
 			set1.add(new ArrayList<Integer>());
 			set2.add(new ArrayList<Integer>());
 			set3.add(new ArrayList<Integer>());
@@ -195,178 +208,195 @@ public class Solution {
 			set5.add(new ArrayList<Integer>());
 			set6.add(new ArrayList<Integer>());
 		}
-		
+
 		List<List<List<Integer>>> sets = new ArrayList<List<List<Integer>>>();
-		
+
 		sets.add(set1);
 		sets.add(set2);
 		sets.add(set3);
 		sets.add(set4);
 		sets.add(set5);
 		sets.add(set6);
-		
-		//assigning elements to each set for each column
-		for(int i=0;i<9;i++) {
+
+		// assigning elements to each set for each column
+		for (int i = 0; i < 9; i++) {
 			List<Integer> li = columns.get(i);
-			for(int j=0;j<6;j++){
-				int randNumIndex = getRand(0, li.size()-1);
+			for (int j = 0; j < 6; j++) {
+				int randNumIndex = getRand(0, li.size() - 1);
 				int randNum = li.get(randNumIndex);
-			
+
 				List<Integer> set = sets.get(j).get(i);
 				set.add(randNum);
-				
+
 				li.remove(randNumIndex);
 			}
 		}
-		
-		//assign element from last column to random set
+
+		// assign element from last column to random set
 		List<Integer> lastCol = columns.get(8);
-		int randNumIndex = getRand(0, lastCol.size()-1);
+		int randNumIndex = getRand(0, lastCol.size() - 1);
 		int randNum = lastCol.get(randNumIndex);
-		
-		int randSetIndex = getRand(0,sets.size()-1);
+
+		int randSetIndex = getRand(0, sets.size() - 1);
 		List<Integer> randSet = sets.get(randSetIndex).get(8);
 		randSet.add(randNum);
-		
+
 		lastCol.remove(randNumIndex);
-		
-		//3 passes over the remaining columns
-		for(int pass=0;pass<3;pass++) {
-			for(int i=0;i<9;i++) {
+
+		// 3 passes over the remaining columns
+		for (int pass = 0; pass < 3; pass++) {
+			for (int i = 0; i < 9; i++) {
 				List<Integer> col = columns.get(i);
-				if(col.size()==0) continue;
-				
-				int randNumIndex_p = getRand(0, col.size()-1);
+				if (col.size() == 0)
+					continue;
+
+				int randNumIndex_p = getRand(0, col.size() - 1);
 				int randNum_p = col.get(randNumIndex_p);
-				
+
 				boolean vacantSetFound = false;
-				while(!vacantSetFound) {
-					int randSetIndex_p = getRand(0,sets.size()-1);
+				while (!vacantSetFound) {
+					int randSetIndex_p = getRand(0, sets.size() - 1);
 					List<List<Integer>> randSet_p = sets.get(randSetIndex_p);
-					
-					if(getNumberOfElementsInSet(randSet_p)==15 || randSet_p.get(i).size()==2) continue;
-					
+
+					if (getNumberOfElementsInSet(randSet_p) == 15 || randSet_p.get(i).size() == 2)
+						continue;
+
 					vacantSetFound = true;
 					randSet_p.get(i).add(randNum_p);
-					
+
 					col.remove(randNumIndex_p);
 				}
 			}
 		}
-		
-		//one more pass over the remaining columns
-		for(int i=0;i<9;i++) {
+
+		// one more pass over the remaining columns
+		for (int i = 0; i < 9; i++) {
 			List<Integer> col = columns.get(i);
-			if(col.size()==0) continue;
-			
-			int randNumIndex_p = getRand(0, col.size()-1);
+			if (col.size() == 0)
+				continue;
+
+			int randNumIndex_p = getRand(0, col.size() - 1);
 			int randNum_p = col.get(randNumIndex_p);
-			
+
 			boolean vacantSetFound = false;
-			while(!vacantSetFound) {
-				int randSetIndex_p = getRand(0,sets.size()-1);
+			while (!vacantSetFound) {
+				int randSetIndex_p = getRand(0, sets.size() - 1);
 				List<List<Integer>> randSet_p = sets.get(randSetIndex_p);
-				
-				if(getNumberOfElementsInSet(randSet_p)==15 || randSet_p.get(i).size()==3) continue;
-				
+
+				if (getNumberOfElementsInSet(randSet_p) == 15 || randSet_p.get(i).size() == 3)
+					continue;
+
 				vacantSetFound = true;
 				randSet_p.get(i).add(randNum_p);
-				
+
 				col.remove(randNumIndex_p);
 			}
 		}
-		
-		//sort the internal sets
-		for(int i=0;i<6;i++){
-			for(int j=0;j<9;j++){
+
+		// sort the internal sets
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 9; j++) {
 				Collections.sort(sets.get(i).get(j));
 			}
 		}
-		
-		
-		//got the sets - need to arrange in tickets now
-		for(int setIndex=0;setIndex<6;setIndex++) {
+
+		// got the sets - need to arrange in tickets now
+		for (int setIndex = 0; setIndex < 6; setIndex++) {
 			List<List<Integer>> currSet = sets.get(setIndex);
 			Node currTicket = nodes[setIndex];
-			
-			//fill first row 
-			for(int size=3;size>0;size--){
-				if(currTicket.getRowCount(0)==5) break;
-				for(int colIndex=0;colIndex<9;colIndex++){
-					if(currTicket.getRowCount(0)==5) break;
-					if(currTicket.A[0][colIndex]!=0) continue;
-					
+
+			// fill first row
+			for (int size = 3; size > 0; size--) {
+				if (currTicket.getRowCount(0) == 5)
+					break;
+				for (int colIndex = 0; colIndex < 9; colIndex++) {
+					if (currTicket.getRowCount(0) == 5)
+						break;
+					if (currTicket.A[0][colIndex] != 0)
+						continue;
+
 					List<Integer> currSetCol = currSet.get(colIndex);
-					if(currSetCol.size()!=size) continue;
-					
-					currTicket.A[0][colIndex]=currSetCol.remove(0);
-	 			}
+					if (currSetCol.size() != size)
+						continue;
+
+					currTicket.A[0][colIndex] = currSetCol.remove(0);
+				}
 			}
-			
-			//fill second row 
-			for(int size=2;size>0;size--){
-				if(currTicket.getRowCount(1)==5) break;
-				for(int colIndex=0;colIndex<9;colIndex++){
-					if(currTicket.getRowCount(1)==5) break;
-					if(currTicket.A[1][colIndex]!=0) continue;
-					
+
+			// fill second row
+			for (int size = 2; size > 0; size--) {
+				if (currTicket.getRowCount(1) == 5)
+					break;
+				for (int colIndex = 0; colIndex < 9; colIndex++) {
+					if (currTicket.getRowCount(1) == 5)
+						break;
+					if (currTicket.A[1][colIndex] != 0)
+						continue;
+
 					List<Integer> currSetCol = currSet.get(colIndex);
-					if(currSetCol.size()!=size) continue;
-					
-					currTicket.A[1][colIndex]=currSetCol.remove(0);
-	 			}
+					if (currSetCol.size() != size)
+						continue;
+
+					currTicket.A[1][colIndex] = currSetCol.remove(0);
+				}
 			}
-			
-			//fill third row 
-			for(int size=1;size>0;size--){
-				if(currTicket.getRowCount(2)==5) break;
-				for(int colIndex=0;colIndex<9;colIndex++){
-					if(currTicket.getRowCount(2)==5) break;
-					if(currTicket.A[2][colIndex]!=0) continue;
-					
+
+			// fill third row
+			for (int size = 1; size > 0; size--) {
+				if (currTicket.getRowCount(2) == 5)
+					break;
+				for (int colIndex = 0; colIndex < 9; colIndex++) {
+					if (currTicket.getRowCount(2) == 5)
+						break;
+					if (currTicket.A[2][colIndex] != 0)
+						continue;
+
 					List<Integer> currSetCol = currSet.get(colIndex);
-					if(currSetCol.size()!=size) continue;
-					
-					currTicket.A[2][colIndex]=currSetCol.remove(0);
-	 			}
-			}		
+					if (currSetCol.size() != size)
+						continue;
+
+					currTicket.A[2][colIndex] = currSetCol.remove(0);
+				}
+			}
 		}
-		
-		try
-		{
+
+		try {
 			// quick patch to ensure columns are sorted
-			for (int i=0; i<6; i++) {
+			for (int i = 0; i < 6; i++) {
 				Node currTicket = nodes[i];
 				currTicket.sortColumns();
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			// something wrong, not a P0...eating the exception
-			System.out.println("Note: there is a small probability your columns may not be sorted, it should not impact the gameplay");
+			System.out.println(
+					"Note: there is a small probability your columns may not be sorted, it should not impact the gameplay");
 			System.out.println("Please create an issue in the github for investigation");
 			System.out.println(e.getMessage());
 		}
 
-		//print the tickets
-		for(int i=0;i<6;i++){
+		// print the tickets
+		for (int i = 0; i < 6; i++) {
 			Node currTicket = nodes[i];
-			
-			for(int r=0;r<3;r++){
-				for(int col=0;col<9;col++){
+
+			for (int r = 0; r < 3; r++) {
+				for (int col = 0; col < 9; col++) {
 					int num = currTicket.A[r][col];
-					if(num!=0) System.out.print(num);
-					
-					if(col!=8) System.out.print(",");
+					if (num != 0)
+						System.out.print(num);
+
+					if (col != 8)
+						System.out.print(",");
 				}
-				if(r!=2) System.out.println();
+				if (r != 2)
+					System.out.println();
 			}
-			
-			if(i!=5){
+
+			if (i != 5) {
 				System.out.println();
 				System.out.println();
 				System.out.println();
 			}
 		}
 	}
+
 }
